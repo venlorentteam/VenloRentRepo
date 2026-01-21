74dedaf56945de176292c0b406241ae9e42793aa
jaycarlos <molokwuchibuzorchristian@gmail.com>
2026-01-21 13:56:30 +0100

    modified the prelim compnents to match venlo current design

--- src/components/Prelim/PrelimFooter.js
+++ src/components/Prelim/PrelimFooter.js
@@ -1,15 +1,27 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import "./PrelimFooter.css"
+
 function PrelimFooter() {
+  const currentYear = new Date().getFullYear()
+  
   return (
-    <div className="footer-links">
-      {/* Should contain footer links for preliminary pages */}
-      <Link to="/about">About</Link>
-      <Link to="/help">Help Center</Link>
-      <Link to="/terms">Terms of Service</Link>
-      <Link to="/privacy-policy">Privacy Policy</Link>
-    </div>
+    <footer className="prelim-footer">
+      <div className="footer-content">
+        {/* Footer Links */}
+        <nav className="footer-links" aria-label="Footer navigation">
+          <Link to="/about" className="footer-link">About</Link>
+          <Link to="/help" className="footer-link">Help Center</Link>
+          <Link to="/terms" className="footer-link">Terms of Service</Link>
+          <Link to="/privacy-policy" className="footer-link">Privacy Policy</Link>
+        </nav>
+        
+        {/* Copyright */}
+        <p className="footer-copyright">
+          © {currentYear} VenloRent. All rights reserved.
+        </p>
+      </div>
+    </footer>
   )
 }
 
