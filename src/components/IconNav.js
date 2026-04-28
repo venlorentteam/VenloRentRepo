d7491012968adcd8ac382124e0a37b1d27d4f43d
Obinabo Walter <walterobinabo@gmail.com>
2026-04-28 11:54:30 +0100

    Committing my local changes before pulling Jay's IconNav changes

--- src/components/IconNav.js
+++ src/components/IconNav.js
@@ -12,7 +12,7 @@ function IconNav() {
       <NavLink to="/dashboard">
         {({ isActive }) => (
           <>
-            <span className="icon">
+            <span className="mobile-icon">
               {isActive ? <GoHomeFill /> : <GoHome />}
             </span>
           </>
@@ -21,7 +21,7 @@ function IconNav() {
       <NavLink to="/search">
         {({ isActive }) => (
           <>
-            <span className="icon">
+            <span className="mobile-icon">
               {isActive ? <RiSearchFill /> : <RiSearchLine />}
             </span>
           </>
@@ -30,7 +30,7 @@ function IconNav() {
       <NavLink to="/create/post-a-request">
         {({ isActive }) => (
           <>
-            <span className="icon">
+            <span className="mobile-icon">
               {isActive ? <FaSquarePlus /> : <FaRegSquarePlus />}
             </span>
           </>
@@ -39,7 +39,7 @@ function IconNav() {
       <NavLink to="/orders">
         {({ isActive }) => (
           <>
-            <span className="icon">
+            <span className="mobile-icon">
               {isActive ? <RiShoppingBag3Fill /> : <RiShoppingBag3Line />}
             </span>
           </>
@@ -48,7 +48,7 @@ function IconNav() {
       <NavLink to="/profile">
         {({ isActive }) => (
           <>
-            <span className="icon">
+            <span className="mobile-icon">
               {isActive ? <FaUserCircle /> : <FaRegUserCircle />}
             </span>
           </>
